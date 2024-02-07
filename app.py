ae082b1e3652e83a65884b455b10b24d657d9dff
LanaKrt <Lanakarout@outlook.com>
2024-02-07 16:16:27 +0100

    Home-Seite eingefügt (von Flask-Demo)

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-#test
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/inquiryoption')
+def inquiry_option():
+    return render_template('inquiryoption.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
