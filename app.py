96537425dca5989b325a41aa999f91915134d8f6
LanaKrt <Lanakarout@outlook.com>
2024-02-07 16:57:31 +0100

    Inquiryoption seite hinzugefügt (von Flask Demo)

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+
 @app.route('/inquiryoption')
 def inquiry_option():
     return render_template('inquiryoption.html')
 
+@app.route('/inquiryform-a')
+def inquiryform_a(): 
+    return render_template('inquiryform-a.html')
+
+@app.route('/inquiryform-t')
+def inquiryform_t():
+    return render_template('inquiryform-t.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
